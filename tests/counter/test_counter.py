f1d232aa1eac61d9131e0455b50b7ed303b70685
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-11-28 10:52:03 -0300

    req 1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,16 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    correct_path = "data/jobs.csv"
+    word1_lc = "javascript"
+    word1_uc = "JAVASCRIPT"
+    word1_mixed="JavaScript"
+    count_word1 = 122
+
+    assert count_ocurrences(correct_path, word1_lc) == count_word1
+    assert count_ocurrences(correct_path, word1_uc) == count_word1
+    assert count_ocurrences(correct_path, word1_mixed) == count_word1
+    assert type(count_ocurrences(correct_path, word1_mixed)) is int 
+
