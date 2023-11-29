8fb922431fe1837965ce7b7b30fb4d971ceb9cc7
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-11-29 16:51:05 -0300

    req 05

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,4 +7,8 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        res_list = []
+        for row in self.jobs_list:
+            if row["industry"] != '' and row["industry"] not in res_list:
+                res_list.append(row["industry"])
+        return(res_list)
