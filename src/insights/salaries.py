4de7c16a3951014be3d75b11ab3470ec967e8005
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-11-29 17:46:45 -0300

    req 06 & 07

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,10 +7,19 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        res_list = []
+        for row in self.jobs_list:
+            if row["max_salary"] != '' and row["max_salary"] != 'invalid' and row["max_salary"] not in res_list:
+                number = row["max_salary"]
+                res_list.append(int(float(number)))
+        return(max(res_list))
 
     def get_min_salary(self) -> int:
-        pass
+        res_list = []
+        for row in self.jobs_list:
+            if row["min_salary"] != '' and row["min_salary"] != 'invalid' and row["min_salary"] not in res_list:
+                res_list.append(int(row["min_salary"]))
+        return(min(res_list))
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
